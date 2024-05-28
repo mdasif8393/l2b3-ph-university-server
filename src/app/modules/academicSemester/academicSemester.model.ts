d862716d99ec83e5babe8318bcbbec35d438ea27
mdasif8393 <md.asif.8393@gmail.com>
2024-05-28 15:54:17 +0600

    add

--- src/app/modules/academicSemester/academicSemester.model.ts
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -39,6 +39,18 @@ const acdemicSemesterSchema = new Schema<TAcademicSemester>(
   },
 );
 
+// middleware
+acdemicSemesterSchema.pre('save', async function (next) {
+  const isSemesterExists = await AcademicSemester.findOne({
+    year: this.year,
+    name: this.name,
+  });
+  if (isSemesterExists) {
+    throw new Error('Semester is already exists');
+  }
+  next();
+});
+
 export const AcademicSemester = model<TAcademicSemester>(
   'AcademicSemester',
   acdemicSemesterSchema,
