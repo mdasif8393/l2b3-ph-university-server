46b17d220636463489fdea1850cada5f99e135fc
mdasif8393 <md.asif.8393@gmail.com>
2024-05-31 13:05:42 +0600

    d

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -18,6 +18,30 @@ const academicDepartmentSchema = new Schema<TAcademicDepartment>(
   },
 );
 
+// Hold create academic department if departnment name already exists
+academicDepartmentSchema.pre('save', async function (next) {
+  const isDepartmentExist = await AcademicDepartment.findOne({
+    name: this.name,
+  });
+  if (isDepartmentExist) {
+    throw new Error('This Academic Department already exists');
+  }
+  next();
+});
+
+// hold update if academic department does not exists
+academicDepartmentSchema.pre('findOneAndUpdate', async function (next) {
+  const query = this.getQuery();
+
+  const isDepartmentExist = await AcademicDepartment.findOne({
+    query,
+  });
+  if (!isDepartmentExist) {
+    throw new Error('This department does not exists');
+  }
+  next();
+});
+
 export const AcademicDepartment = model<TAcademicDepartment>(
   'AcademicDepartment',
   academicDepartmentSchema,
