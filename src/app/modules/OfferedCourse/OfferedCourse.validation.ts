cb18c06e0e2299da2746d3bc69525f4e5748206e
mdasif8393 <md.asif.8393@gmail.com>
2024-06-09 01:04:31 +0600

    create offered course

--- src/app/modules/OfferedCourse/OfferedCourse.validation.ts
+++ src/app/modules/OfferedCourse/OfferedCourse.validation.ts
@@ -2,18 +2,48 @@ import { z } from 'zod';
 import { Days } from './OfferedCourse.constant';
 
 const createOfferedCourseValidationSchema = z.object({
-  body: z.object({
-    semesterRegistration: z.string(),
-    academicFaculty: z.string(),
-    academicDepartment: z.string(),
-    course: z.string(),
-    faculty: z.string(),
-    section: z.number(),
-    maxCapacity: z.number(),
-    days: z.array(z.enum([...Days] as [string, ...string[]])),
-    startTime: z.string(),
-    endTime: z.string(),
-  }),
+  body: z
+    .object({
+      semesterRegistration: z.string(),
+      academicFaculty: z.string(),
+      academicDepartment: z.string(),
+      course: z.string(),
+      faculty: z.string(),
+      section: z.number(),
+      maxCapacity: z.number(),
+      days: z.array(z.enum([...Days] as [string, ...string[]])),
+      startTime: z.string().refine(
+        (time) => {
+          const regex = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/;
+          return regex.test(time);
+        },
+        {
+          message: 'Invalid time format, expected "HH:MM" in 24H format ',
+        },
+      ), // HH:MM 00-23: 00-59
+      endTime: z.string().refine(
+        (time) => {
+          const regex = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/;
+          return regex.test(time);
+        },
+        {
+          message: 'Invalid time format, expected "HH:MM" in 24H format ',
+        },
+      ),
+    })
+    .refine(
+      (body) => {
+        // start time : 10:30 => 1970-01-01T10:30:00
+        // End time : 12:30 => 1970-01-01T12:30:00
+        const start = new Date(`1970-01-01T${body.startTime}:00`);
+        const end = new Date(`1970-01-01T${body.endTime}:00`);
+
+        return end > start;
+      },
+      {
+        message: 'Start time should be before end time',
+      },
+    ),
 });
 
 const updateOfferedCourseValidationSchema = z.object({
