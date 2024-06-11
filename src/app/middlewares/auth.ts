01b4abe00939a2a15df83b20a0a095fcff67203c
mdasif8393 <md.asif.8393@gmail.com>
2024-06-11 12:53:02 +0600

    add jwt toke access for admin

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -3,9 +3,10 @@ import httpStatus from 'http-status';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import config from '../config';
 import AppError from '../errors/AppError';
+import { TUserRole } from '../modules/user/user.interface';
 import catchAsync from '../utils/catchAsync';
 
-const auth = () => {
+const auth = (...requiredRoles: TUserRole[]) => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization;
 
@@ -23,6 +24,11 @@ const auth = () => {
           throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized');
         }
 
+        const role = (decoded as JwtPayload).role;
+
+        if (requiredRoles && !requiredRoles.includes(role)) {
+          throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized');
+        }
         req.user = decoded as JwtPayload;
         next();
       },
